cee3c5512168013109fe2a2883bc44e5dea88597
Alberto Bongioanni <a.bongioanni.0746@vallauri.edu>
2020-05-12 10:18:16 +0200

    dfg

--- server/login.php
+++ server/login.php
@@ -4,20 +4,20 @@ require("_libreria.php");
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     //controllo parametri
-    if (!isset($_POST["username"]) || !isset($_POST["password"])) {
+    if (!isset($_POST["email"]) || !isset($_POST["password"])) {
         http_response_code(422);
         die("Parametro mancante.");
     }
 
     $con = _connection("clashroom");
-    $username = $con->real_escape_string($_POST["username"]);
+    $email = $con->real_escape_string($_POST["email"]);
     $password = $con->real_escape_string($_POST["password"]);
 
     $sql = "SELECT * FROM Correntisti WHERE email='$email';";
     $data = _eseguiQuery($con, $sql);
     if (count($data) == 0) {
         http_response_code(401);
-        die("Username inesistente");
+        die("email inesistente");
     } else {
         if ($data[0]["password"] == $password) {
             $aus=array("ris"=>"ok");
