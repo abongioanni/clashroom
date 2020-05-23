cb7e0d02702eb66f62f7d9d99d77ac36ef10a56e
Alberto Bongioanni <61777787+abongioanni@users.noreply.github.com>
2020-05-23 10:25:45 +0200

    Sito funzionante 1.4

--- clashroom.sql
+++ clashroom.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Creato il: Mag 22, 2020 alle 08:26
+-- Creato il: Mag 23, 2020 alle 10:25
 -- Versione del server: 10.4.11-MariaDB
--- Versione PHP: 7.4.4
+-- Versione PHP: 7.4.5
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -35,6 +35,15 @@ CREATE TABLE `courses` (
   `creatorId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dump dei dati per la tabella `courses`
+--
+
+INSERT INTO `courses` (`id`, `nome`, `creatorId`) VALUES
+(1, 'Informatica', 1),
+(2, 'Sistemi', 1),
+(3, 'Italiano', 2);
+
 -- --------------------------------------------------------
 
 --
@@ -49,6 +58,17 @@ CREATE TABLE `events` (
   `courseId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dump dei dati per la tabella `events`
+--
+
+INSERT INTO `events` (`id`, `argomento`, `do`, `teacherId`, `courseId`) VALUES
+(10, 'Reti prova', '2020-05-18 10:50:00', 1, 2),
+(11, 'Interrogazione e verifica di recupero', '2020-05-21 12:30:00', 1, 1),
+(12, 'Interrogazione e verifica di recupero', '2020-05-21 12:30:00', 1, 1),
+(13, 'Interrogazione e verifica di recupero', '2020-05-21 12:30:00', 1, 1),
+(14, 'Interrogazione e verifica di recupero', '2020-05-20 12:30:00', 1, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -61,6 +81,13 @@ CREATE TABLE `studsub` (
   `courseId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Dump dei dati per la tabella `studsub`
+--
+
+INSERT INTO `studsub` (`id`, `studId`, `courseId`) VALUES
+(15, 7, 2);
+
 -- --------------------------------------------------------
 
 --
@@ -72,9 +99,7 @@ CREATE TABLE `user` (
   `nome` varchar(255) NOT NULL,
   `cognome` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `dob` date NOT NULL,
   `email` varchar(255) NOT NULL,
-  `foto` varchar(255) NOT NULL,
   `st` tinyint(1) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -115,25 +140,25 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT per la tabella `courses`
 --
 ALTER TABLE `courses`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- AUTO_INCREMENT per la tabella `events`
 --
 ALTER TABLE `events`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT per la tabella `studsub`
 --
 ALTER TABLE `studsub`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT per la tabella `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
